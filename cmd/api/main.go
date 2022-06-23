b1a6fc55fedcd9f11348384ae2651dc39ecda798
Demby <dmcchy@gmail.com>
2022-06-23 14:56:15 +0800

    Added REST API initial template

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,8 +3,48 @@ package main
 import (
 	"fmt"
 	"github.com/dembygenesis/platform_engineer_exam/dependency_injection/dic"
+	"github.com/dembygenesis/platform_engineer_exam/src/config"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"log"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
 )
+
+// initAPI boots our REST API connections
+func initAPI(config *config.Config) {
+	app := fiber.New(fiber.Config{
+		BodyLimit: 20971520,
+	})
+
+	app.Use(recover.New())
+	app.Use(logger.New(logger.Config{
+		Format:     "${pid} ${status} - ${method} ${path}\n",
+		TimeFormat: "02-Jan-2006",
+		TimeZone:   "America/New_York",
+	}))
+
+	app.Static("/", "./public")
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-shutdown
+		fmt.Println("Gracefully shutting down")
+		err := app.Shutdown()
+		if err != nil {
+			fmt.Println("Shutting down error", err)
+		}
+	}()
+
+	if err := app.Listen(":" + strconv.Itoa(config.API.Port)); err != nil {
+		fmt.Println("gg has error", err)
+	}
+}
 
 func main() {
 	builder, err := dic.NewBuilder()
@@ -15,4 +55,6 @@ func main() {
 	ctn := builder.Build()
 	cfg, _ := ctn.SafeGetConfig()
 	fmt.Println(cfg)
+
+	initAPI(cfg)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/dembygenesis/platform_engineer_exam
 go 1.18
 
 require (
+	github.com/gofiber/fiber/v2 v2.34.1
 	github.com/pkg/errors v0.9.1
 	github.com/sarulabs/di/v2 v2.4.2
 	github.com/sarulabs/dingo/v4 v4.2.0
@@ -10,8 +11,10 @@ require (
 )
 
 require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
@@ -21,6 +24,9 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.4.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.37.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
 	golang.org/x/sys v0.0.0-20220622161953-175b2fd9d664 // indirect
 	golang.org/x/text v0.3.7 // indirect
