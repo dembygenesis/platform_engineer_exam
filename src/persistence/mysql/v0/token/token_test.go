92ffbc86ad2b755bd2108322ead5cd54cac18266
Demby <dmcchy@gmail.com>
2022-06-25 18:57:52 +0800

    Finished sqldata mock tests for Generate()

--- src/persistence/mysql/v0/token/token_test.go
+++ src/persistence/mysql/v0/token/token_test.go
@@ -10,40 +10,6 @@ import (
 	"time"
 )
 
-var (
-// I'll have a bunch of mocks here
-)
-
-// I'll have an assembler function here and combines these mocks,
-// and pushes them to my mock function
-func validatePassFetchToken(mock sqlmock.Sqlmock) {
-	/*sqlInsertToken := `
-
-	`*/
-}
-
-/*func TestPersistenceToken_GenerateFailCheckUniqueToken_ForTestsIgnore(t *testing.T) {
-	db, mock, err := sqlmock.New()
-
-	// Play with mock
-	row := sqlmock.NewRows([]string{"id", "name"})
-	row.AddRow(999, "abc")
-
-	sqlToken := "SELECT `token`.* FROM `token` WHERE (`token`.`key` = ?);"
-	mock.ExpectQuery(regexp.QuoteMeta(sqlToken)).WithArgs("123").WillReturnRows(row)
-	// Play with mock
-
-	persistenceToken := PersistenceToken{db: db}
-	_, err = persistenceToken.Generate(context.Background(), 3)
-	if err != nil {
-		fmt.Println("=========== err ===========", err)
-		require.Error(t, err, errCheckUniqueToken)
-		errMsg := err.Error()
-		wantErrMsg := errCheckUniqueToken.Error()
-		assert.Containsf(t, errMsg, wantErrMsg, "expected error containing %q, got %s", wantErrMsg, err)
-	}
-}*/
-
 func configureMockGenerateFailFetchToken(mock sqlmock.Sqlmock) {
 	sqlToken := "SELECT (.+) FROM `token` WHERE .*"
 	mock.ExpectQuery(regexp.QuoteMeta(sqlToken)).WithArgs("123").WillReturnError(errFetchToken)
@@ -61,6 +27,18 @@ func configureMockGeneratePassFetchToken(mock sqlmock.Sqlmock, randomString stri
 		"expires_at",
 	})
 	mock.ExpectQuery(regexp.QuoteMeta(sqlToken)).WithArgs(randomString).WillReturnRows(rows)
+}
+
+func configureMockGenerateFailInsertToken(mock sqlmock.Sqlmock, randomString string, createdAt time.Time) {
+	var mockIdReturned int64 = 1
+	sqlInsert := "INSERT INTO `token` (`key`,`created_at`,`created_by`,`expires_at`) VALUES (?,?,?,?)"
+	mock.ExpectExec(regexp.QuoteMeta(sqlInsert)).WithArgs(
+		randomString,
+		createdAt,
+		3,
+		createdAt.Add(7*time.Hour*24),
+	).WillReturnResult(sqlmock.NewResult(mockIdReturned, 1)).WillReturnError(errInsertNewToken)
+
 }
 
 func configureMockGeneratePassInsertToken(mock sqlmock.Sqlmock, randomString string, createdBy int, createdAt time.Time) {
@@ -115,6 +93,20 @@ func TestPersistenceToken_Generate_FailCheckUniqueToken(t *testing.T) {
 	assert.Containsf(t, errMsg, wantErrMsg, "expected error containing %q, got %s", wantErrMsg, err)
 }
 
-func TestPersistenceToken_GenerateErrInsertNewToken(t *testing.T) {
+func TestPersistenceToken_Generate_FailInsertNewToken(t *testing.T) {
+	randomString := generateRandomCharacters(12)
+	createdAt := time.Now()
+	createdById := 3
 
+	db, mock, err := sqlmock.New()
+	configureMockGeneratePassFetchToken(mock, randomString)
+	configureMockGenerateFailInsertToken(mock, randomString, createdAt)
+
+	persistenceToken := PersistenceToken{db: db}
+	_, err = persistenceToken.Generate(context.Background(), createdById, randomString, &createdAt)
+	require.Error(t, err)
+
+	errMsg := err.Error()
+	wantErrMsg := errInsertNewToken.Error()
+	assert.Containsf(t, errMsg, wantErrMsg, "expected error containing %q, got %s", wantErrMsg, err)
 }
