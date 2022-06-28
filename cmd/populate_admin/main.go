dd67cf84f5ac8508179665c9cbd223a41ec5fddc
Demby Abella <dmcchy@gmail.com>
2022-06-28 20:36:08 +0800

    Improved windows compabitibility

--- cmd/populate_admin/main.go
+++ cmd/populate_admin/main.go
@@ -16,7 +16,6 @@ import (
 )
 
 func main() {
-	fmt.Println("=============== 1")
 	logger := common.GetLogger(context.Background())
 	builder, err := dic.NewBuilder()
 	if err != nil {
@@ -24,23 +23,19 @@ func main() {
 	}
 	ctn := builder.Build()
 
-	fmt.Println("=============== 2")
 	mysqlConnection, err := ctn.SafeGetMysqlConnection()
 	if err != nil {
 		log.Fatalf("error getting the mysql_connection from the container: %v", err.Error())
 	}
 
-	fmt.Println("=============== 3")
 	name := "Admin User"
 	email := "admin@gmail.com"
 	unhashedPassword := "123456"
 	password, _ := strings.Encrypt(unhashedPassword)
-	fmt.Println("=============== 4")
 	if err = bcrypt.CompareHashAndPassword([]byte(password), []byte(unhashedPassword)); err != nil {
 		log.Fatalf("Failed to synchronize passwords: %v", err)
 	}
 
-	fmt.Println("=============== 5")
 	newUser := models_schema.User{
 		Name:     name,
 		Email:    email,
@@ -50,14 +45,12 @@ func main() {
 	if err != nil {
 		log.Fatalf("error inserting a dummy admin user: %v", err.Error())
 	}
-	fmt.Println("=============== 6")
-	logger.WithFields(logrus.Fields{
-		email:    "admin@gmamil.com",
-		password: "123456",
-	})
 
-	/*fmt.Println("Successfully added a new admin with credentials of", map[string]string{
-		email:    "admin@gmamil.com",
+	logger.WithFields(logrus.Fields{
+		msg: "Successfully added a new admin user",
+		email:    "admin@gmail.com",
 		password: "123456",
-	})*/
+	}).Info("add_admin_user")
+
+
 }
